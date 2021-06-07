229deba6e7df3c1a74e5beedc8b159e562a85462
Paulo Alenquer <pauloalenquer@gmail.com>
2021-06-07 14:27:57 -0300

    transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,10 @@
 import logoImg from "../../assets/logo.svg";
 
-export function Header() {
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <header className="bg-purple-600 w-full text-white">
       <div className="m-auto container max-w-screen-lg flex px-2 pb-44 pt-4 items-center justify-between">
@@ -9,7 +13,11 @@ export function Header() {
           <span className="font-semibold text-lg mb-1">pa money</span>
         </div>
 
-        <button className="text-sm md:text-md bg-purple-500 px-8 rounded h-10 transition duration-300 filter hover:brightness-90" type="button">
+        <button
+          className="text-sm md:text-md bg-purple-500 px-8 rounded h-10 transition duration-300 filter hover:brightness-90"
+          type="button"
+          onClick={onOpenNewTransactionModal}
+        >
           Nova Transação
         </button>
       </div>
